bf11cfd84b928c1197aed3b03ce399e016aa13b5
sercasor <sergcasor@gmail.com>
2026-03-15 10:48:00 +0100

    orderdetails repo added, no custom methods used

--- back-end/src/main/java/com/project/code/Repo/OrderDetailsRepository.java
+++ back-end/src/main/java/com/project/code/Repo/OrderDetailsRepository.java
@@ -1,14 +1,12 @@
 package com.project.code.Repo;
 
+import com.project.code.Model.OrderDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface OrderDetailsRepository {
-// 1. Add the repository interface:
-//    - Extend JpaRepository<OrderDetails, Long> to inherit basic CRUD functionality.
-//    - This allows the repository to perform operations like save, delete, update, and find without having to implement these methods manually.
-
-// Example: public interface OrderDetailsRepository extends JpaRepository<OrderDetails, Long> {}
-
-// 2. Since no custom methods are required for this repository, the default CRUD operations (save, delete, update, findById, etc.) are available out of the box.
+@Repository
+public interface OrderDetailsRepository extends JpaRepository<OrderDetails,Long> {
+//no need to add custom methods for basic CRUD
 
 }
 
