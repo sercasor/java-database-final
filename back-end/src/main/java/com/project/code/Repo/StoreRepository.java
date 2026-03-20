680c09aafe80632f353996c15e8e929f4edb6860
sercasor <sergcasor@gmail.com>
2026-03-20 19:02:23 +0100

    basic store repo done with findByID commented out and findByNameLikeIgnoreCase

--- back-end/src/main/java/com/project/code/Repo/StoreRepository.java
+++ back-end/src/main/java/com/project/code/Repo/StoreRepository.java
@@ -1,6 +1,49 @@
 package com.project.code.Repo;
 
-public interface StoreRepository {
+import com.project.code.Model.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StoreRepository extends JpaRepository<Store,Long> {
+
+    /**
+     * Retrieve stores whose name contains a given substring.
+     * @param name
+     * @return
+     */
+    public List<Store> findByNameLikeIgnoreCase(String name);
+
+    // public List<Store> findById(Long ID);
+//  shouldn't be needed, just here for reference in case de ID generic is problematic so overriding the method is required
+   
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // 1. Add the repository interface:
 //    - Extend JpaRepository<Store, Long> to inherit basic CRUD functionality.
 //    - This allows the repository to perform operations like save, delete, update, and find without having to implement these methods manually.
@@ -20,7 +63,3 @@ public interface StoreRepository {
 //      - Return type: List<Store>
 //      - Parameter: String pname
 //      - Use @Query annotation to write a custom query.
-
-   
-
-}
